7c5e6e2afbc11ba03d1b5da0a0b9712693fbc7b9
UO270930 <marcostobias64@gmail.com>
2020-11-19 18:58:39 +0100

    aaaaa

---
+++ js/PRACTICA-3/Ejercicio-09/Ejercicio9.js
@@ -0,0 +1,135 @@
+class Meteo {
+    constructor(){
+        this.apikey = "290fd3ced9bb2176b491526dd1208f09";
+        this.codigoPais = "ES";
+        this.unidades = "&units=metric";
+        this.idioma = "&lang=es";
+        this.ciudad = "Cuenca";
+        this.tipo = "&mode=xml";
+        this.url = "http://api.openweathermap.org/data/2.5/weather?q=" + this.ciudad + this.tipo + this.unidades + this.idioma + "&APPID=" + this.apikey;
+    }
+    cargarDatos(id){
+        $.ajax({
+            dataType: "xml",
+            url: this.url,
+            method: 'GET',
+            success: function(datos){
+
+                    var ciudad                = $('city',datos).attr("name");
+                    var longitud              = $('coord',datos).attr("lon");
+                    var latitud               = $('coord',datos).attr("lat");
+                    var pais                  = $('country',datos).text();
+                    var amanecer              = $('sun',datos).attr("rise");
+                    var minutosZonaHoraria    = new Date().getTimezoneOffset();
+                    var amanecerMiliSeg1970   = Date.parse(amanecer);
+                        amanecerMiliSeg1970  -= minutosZonaHoraria * 60 * 1000;
+                    var amanecerLocal         = (new Date(amanecerMiliSeg1970)).toLocaleTimeString("es-ES");
+                    var oscurecer             = $('sun',datos).attr("set");          
+                    var oscurecerMiliSeg1970  = Date.parse(oscurecer);
+                        oscurecerMiliSeg1970  -= minutosZonaHoraria * 60 * 1000;
+                    var oscurecerLocal        = (new Date(oscurecerMiliSeg1970)).toLocaleTimeString("es-ES");
+                    var temperatura           = $('temperature',datos).attr("value");
+                    var temperaturaMin        = $('temperature',datos).attr("min");
+                    var temperaturaMax        = $('temperature',datos).attr("max");
+                    var temperaturaUnit       = $('temperature',datos).attr("unit");
+                    var humedad               = $('humidity',datos).attr("value");
+                    var humedadUnit           = $('humidity',datos).attr("unit");
+                    var presion               = $('pressure',datos).attr("value");
+                    var presionUnit           = $('pressure',datos).attr("unit");
+                    var velocidadViento       = $('speed',datos).attr("value");
+                    var nombreViento          = $('speed',datos).attr("name");
+                    var direccionViento       = $('direction',datos).attr("value");
+                    var codigoViento          = $('direction',datos).attr("code");
+                    var nombreDireccionViento = $('direction',datos).attr("name");
+                    var nubosidad             = $('clouds',datos).attr("value");
+                    var nombreNubosidad       = $('clouds',datos).attr("name");
+                    var visibilidad           = $('visibility',datos).attr("value");
+                    var precipitacionValue    = $('precipitation',datos).attr("value");
+                    var precipitacionMode     = $('precipitation',datos).attr("mode");
+                    var descripcion           = $('weather',datos).attr("value");
+                    var icono                 = $('weather',datos).attr("icon");
+                    var horaMedida            = $('lastupdate',datos).attr("value");
+                    var horaMedidaMiliSeg1970 = Date.parse(horaMedida);
+                        horaMedidaMiliSeg1970 -= minutosZonaHoraria * 60 * 1000;
+                    var horaMedidaLocal       = (new Date(horaMedidaMiliSeg1970)).toLocaleTimeString("es-ES");
+                    var fechaMedidaLocal      = (new Date(horaMedidaMiliSeg1970)).toLocaleDateString("es-ES");
+
+                    var stringDatos =  "<img src=http://openweathermap.org/img/wn/" + icono + "@2x.png alt=\"icono del Tiempo\"/>";
+                        stringDatos += "<ul><li>Ciudad: " + ciudad + "</li>";
+                        stringDatos += "<li>Longitud: " + longitud + " grados</li>";
+                        stringDatos += "<li>Latitud: " + latitud + " grados</li>";
+                        stringDatos += "<li>País: " + pais + "</li>";
+                        stringDatos += "<li>Amanece a las: " + amanecerLocal + "</li>";
+                        stringDatos += "<li>Oscurece a las: " + oscurecerLocal + "</li>";
+                        stringDatos += "<li>Temperatura: " + temperatura + " grados Celsius</li>";
+                        stringDatos += "<li>Temperatura mínima: " + temperaturaMin + " grados Celsius</li>";
+                        stringDatos += "<li>Temperatura máxima: " + temperaturaMax + " grados Celsius</li>";
+                        stringDatos += "<li>Temperatura (unidades): " + temperaturaUnit + "</li>";
+                        stringDatos += "<li>Humedad: " + humedad + " " + humedadUnit + "</li>";
+                        stringDatos += "<li>Presión: " + presion + " " + presionUnit + "</li>";
+                        stringDatos += "<li>Velocidad del viento: " + velocidadViento + " metros/segundo</li>";
+                        stringDatos += "<li>Nombre del viento: " + nombreViento + "</li>";
+                        stringDatos += "<li>Dirección del viento: " + direccionViento + " grados</li>";
+                        stringDatos += "<li>Código del viento: " + codigoViento + "</li>";
+                        stringDatos += "<li>Nombre del viento: " + nombreDireccionViento + "</li>";
+                        stringDatos += "<li>Nubosidad: " + nubosidad + "</li>";
+                        stringDatos += "<li>Nombre nubosidad: " + nombreNubosidad + "</li>";
+                        stringDatos += "<li>Visibilidad: " + visibilidad + " metros</li>";
+                        stringDatos += "<li>Precipitación valor: " + precipitacionValue + "</li>";
+                        stringDatos += "<li>Precipitación modo: " + precipitacionMode + "</li>";
+                        stringDatos += "<li>Descripción: " + descripcion + "</li>";
+                        stringDatos += "<li>Hora de la medida: " + horaMedidaLocal + "</li>";
+                        stringDatos += "<li>Fecha de la medida: " + fechaMedidaLocal + "</li></ul>";
+                    
+                    $("#p" + id).html(stringDatos);
+                },
+            error:function(){
+                $("h4" + id).remove();
+                $("p" + id).remove();
+                }
+        });
+    }
+    crearElemento(tipoElemento, texto, insertarAntesDe, id){
+        // Crea un nuevo elemento modificando el Ã¡rbol DOM
+        // El elemnto creado es de 'tipoElemento' con un 'texto' 
+        // El elemnto se coloca antes del elemnto 'insertarAntesDe'
+        var elemento = document.createElement(tipoElemento); 
+        elemento.innerHTML = texto;
+        elemento.setAttribute("id", tipoElemento + id);
+        $(insertarAntesDe).before(elemento);
+    }
+
+    mostrar() {
+        this.mostrarCuenca();
+        this.mostrarLlanes();
+        this.mostrarFelechosa();
+    }
+
+    mostrarCuenca() {
+        this.crearElemento("h4","Datos","#llanes", 1); // Crea un elemento con DOM 
+        this.crearElemento("p","","#llanes", 1); // Crea un elemento con DOM para los datos obtenidos con JSON
+
+        this.cargarDatos(1);
+    }
+
+    mostrarLlanes() {
+        this.crearElemento("h4","Datos","#felechosa", 2); // Crea un elemento con DOM 
+        this.crearElemento("p","","#felechosa", 2); // Crea un elemento con DOM para los datos obtenidos con JSON
+
+        this.ciudad = "Llanes";
+        this.url = "http://api.openweathermap.org/data/2.5/weather?q=" + this.ciudad + this.tipo + this.unidades + this.idioma + "&APPID=" + this.apikey;
+
+        this.cargarDatos(2);
+    }
+
+    mostrarFelechosa() {
+        this.crearElemento("h4","Datos","#footer", 3); // Crea un elemento con DOM 
+        this.crearElemento("p","","#footer", 3); // Crea un elemento con DOM para los datos obtenidos con JSON
+
+        this.ciudad = "Felechosa";
+        this.url = "http://api.openweathermap.org/data/2.5/weather?q=" + this.ciudad + this.tipo + this.unidades + this.idioma + "&APPID=" + this.apikey;
+
+        this.cargarDatos(3);
+    }
+}
+var meteo = new Meteo();
---
+++ js/PRACTICA-3/Ejercicio-10/Ejercicio10.js
@@ -0,0 +1,75 @@
+class Noticias {
+    constructor(){
+        this.apikey = "40e57a011b9f4af8af52aac285232f11";
+        this.codigoPais = "country=es&";
+        this.url = 'http://newsapi.org/v2/top-headlines?country=us&apiKey=40e57a011b9f4af8af52aac285232f11';
+    }
+    cargarDatos(){
+        $.ajax({
+            dataType: "JSON",
+            url: this.url,
+            method: 'GET',
+            mode: 'no-cors',
+            credentials: 'same-origin',
+            success: function(datos){
+                    $("pre").text(JSON.stringify(datos, null, 2));
+                    
+                    
+                    //$("#p" + id).html(stringDatos);
+                },
+            error:function(){
+                $("pre").remove();
+                //$("h4" + id).remove();
+                //$("p" + id).remove();
+                }
+        });
+    }
+    crearElemento(tipoElemento, texto, insertarAntesDe){
+        var elemento = document.createElement(tipoElemento); 
+        elemento.innerHTML = texto;
+        $(insertarAntesDe).before(elemento);
+    }
+
+    mostrar() {
+        //this.crearElemento("h4","Datos","#footer");
+        //this.crearElemento("p","","#footer");
+        //this.crearElemento("pre", "", "#footer");
+        //this.cargarDatos();
+
+        var url = "http://api.openweathermap.org/data/2.5/weather?q=Cuenca,ES&units=metric&lang=es&APPID=290fd3ced9bb2176b491526dd1208f09";
+        var url = 'http://newsapi.org/v2/top-headlines?' +
+          'country=us&' +
+          'apiKey=40e57a011b9f4af8af52aac285232f11';
+        /* var req = new Request(url);
+        fetch(req)
+         .then(function(response) {
+        console.log(response.json());
+        }) */
+
+        /* var aux = fetch(url, {
+            mode: 'no-cors',
+            credentials: 'same-origin'
+            }).then(function(response){
+                console.log(response.json());
+            }); */
+
+            /* var aux = fetch(url, {
+                mode: 'no-cors',
+                credentials: 'same-origin'
+                }); */
+
+                $.ajax({
+                    url:
+                      url,
+                    method: "GET",
+                    error: function() {
+                      console.log("fucked");
+                    },
+                    success: function(data) {
+                      processData(data);
+                    }
+                  });
+    
+    }
+}
+var noticias = new Noticias();
