e7ccad5ffaa9653e493dc0cf9b3534d597c78758
UO270930 <marcostobias64@gmail.com>
2020-12-01 12:09:26 +0100

    ss

--- js/PRACTICA-3/Ejercicio-11/Tarea-6/Ejercicio11.js
+++ js/PRACTICA-3/Ejercicio-11/Tarea-6/Ejercicio11.js
@@ -1,42 +1,88 @@
-class MapaDinamicoGoogle {
-    constructor(){}
+class MapaMaravilla {
+  constructor() {
+      this.current = "giza";
+   }
 
-    initMap(){  
-        var centro = {lat: 43.3672702, lng: -5.8502461};
-        var mapaGeoposicionado = new google.maps.Map(document.getElementById('mapa'),{
-            zoom: 8,
-            center:centro,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        });
-        
-        var infoWindow = new google.maps.InfoWindow;
-        if (navigator.geolocation) {
-              navigator.geolocation.getCurrentPosition(function(position) {
-                var pos = {
-                  lat: position.coords.latitude,
-                  lng: position.coords.longitude
-                };
-    
-                infoWindow.setPosition(pos);
-                infoWindow.setContent('Localización encontrada');
-                infoWindow.open(mapaGeoposicionado);
-                mapaGeoposicionado.setCenter(pos);
-              }, function() {
-                handleLocationError(true, infoWindow, mapaGeoposicionado.getCenter());
-              });
-            } else {
-              // Browser doesn't support Geolocation
-              handleLocationError(false, infoWindow, mapaGeoposicionado.getCenter());
-            }
-          }
-    
-          handleLocationError(browserHasGeolocation, infoWindow, pos) {
-            infoWindow.setPosition(pos);
-            infoWindow.setContent(browserHasGeolocation ?
-                                  'Error: Ha fallado la geolocalización' :
-                                  'Error: Su navegador no soporta geolocalización');
-            infoWindow.open(mapaGeoposicionado);
-          }
+  initMap() {
+    this.closeButGiza();
+    this.openGiza();
+  }
+
+  closeButGiza() {
+    $("#chichen").toggle();
+    $("#coliseo").toggle();
+    $("#muralla").toggle();
+    $("#taj").toggle();
+  }
+
+  openGiza() {
+    this.closeCurrent();
+    $("#giza").toggle();
+    this.current = "giza";
+
+    var centro = { lat: 29.978904, lng: 31.133078 };
+    var mapaGeoposicionado = new google.maps.Map(document.getElementById('mapa'), {
+      zoom: 16,
+      center: centro,
+      mapTypeId: google.maps.MapTypeId.SATELLITE
+    });
+  }
+
+  openChichen() {
+    this.closeCurrent();
+    $("#chichen").toggle();
+    this.current = "chichen";
+
+    var centro = { lat: 20.683437, lng: -88.568623 };
+    var mapaGeoposicionado = new google.maps.Map(document.getElementById('mapa'), {
+      zoom: 17,
+      center: centro,
+      mapTypeId: google.maps.MapTypeId.SATELLITE
+    });
+  }
+
+  openColiseo() {
+    this.closeCurrent();
+    $("#coliseo").toggle();
+    this.current = "coliseo";
+
+    var centro = { lat: 41.890043, lng: 12.492140 };
+    var mapaGeoposicionado = new google.maps.Map(document.getElementById('mapa'), {
+      zoom: 17,
+      center: centro,
+      mapTypeId: google.maps.MapTypeId.SATELLITE
+    });
+  }
+
+  openMuralla() {
+    this.closeCurrent();
+    $("#muralla").toggle();
+    this.current = "muralla";
+
+    var centro = { lat: 40.431783, lng: 116.571924 };
+    var mapaGeoposicionado = new google.maps.Map(document.getElementById('mapa'), {
+      zoom: 17,
+      center: centro,
+      mapTypeId: google.maps.MapTypeId.SATELLITE
+    });
+  }
+
+  openTaj() {
+    this.closeCurrent();
+    $("#taj").toggle();
+    this.current = "taj";
+
+    var centro = { lat: 27.174044, lng: 78.041939 };
+    var mapaGeoposicionado = new google.maps.Map(document.getElementById('mapa'), {
+      zoom: 16,
+      center: centro,
+      mapTypeId: google.maps.MapTypeId.SATELLITE
+    });
+  }
+
+  closeCurrent() {
+    $("#" + this.current).toggle();
+  }
 }
 
-var mapaDinamicoGoogle = new MapaDinamicoGoogle();
+var mapaMaravilla = new MapaMaravilla();
